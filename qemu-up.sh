2638b275e98fd59009974594d8a9f8e2fc5883ea
Miguel Xavier <1868619+miguelxvr@users.noreply.github.com>
2024-03-27 21:36:52 +0000

    Custom scripts

---
+++ qemu-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sudo qemu-system-i386 \
+#  --kernel output/images/bzImage \
+#  --hda output/images/rootfs.ext2 \
+#  --nographic \
+#  --append "console=ttyS0 root=/dev/sda"
+
+sudo qemu-system-i386 \
+  --device e1000,netdev=eth0,mac=aa:bb:cc:dd:ee:ff \
+	--netdev tap,id=eth0,script=custom-scripts/qemu-ifup \
+	--kernel output/images/bzImage \
+	--hda output/images/rootfs.ext2 \
+	--nographic \
+	--append "console=ttyS0 root=/dev/sda" 
