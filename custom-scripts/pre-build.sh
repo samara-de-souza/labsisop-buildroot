acd0de6f146440fc9c7b904022f80007417530cb
Miguel Xavier <1868619+miguelxvr@users.noreply.github.com>
2024-04-05 22:00:15 +0000

    Custom scripts

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+export PATH=$PATH:$BASE_DIR/../output/host/bin
+
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+i686-linux-gcc $BASE_DIR/../custom-scripts/hello.c -O2 -o $BASE_DIR/../output/target/usr/bin/hello
